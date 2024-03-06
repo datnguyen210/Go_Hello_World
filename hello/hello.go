ac8f1685712c69f9d52444386324966fe5c0e39b
Dat Nguyen <datnguyenftu210@gmail.com>
2024-03-07 05:47:25 +0700

    fix: return meaningful error message

--- hello/hello.go
+++ hello/hello.go
@@ -11,7 +11,7 @@ func main() {
     // Set properties of the predefined Logger, including
     // the log entry prefix and a flag to disable printing
     // the time, source file, and line number.
-    log.SetPrefix("greetings: ")
+    log.SetPrefix("Something went wrong: ")
     log.SetFlags(0)
 
     // Request a greeting message.
