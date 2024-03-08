c1364e1e51b0446f6951a0094580c99d4ad871c3
Dat Nguyen <datnguyenftu210@gmail.com>
2024-03-08 07:12:07 +0700

    feat: use map to return multiple greeting messages

--- hello/hello.go
+++ hello/hello.go
@@ -11,18 +11,18 @@ func main() {
     // Set properties of the predefined Logger, including
     // the log entry prefix and a flag to disable printing
     // the time, source file, and line number.
-    log.SetPrefix("Something went wrong: ")
+    log.SetPrefix("greetings: ")
     log.SetFlags(0)
 
-    // Request a greeting message.
-    message, err := greetings.Hello("Peter")
-    // If an error was returned, print it to the console and
-    // exit the program.
+    // A slice of names.
+    names := []string{"Gladys", "Samantha", "Darrin"}
+
+    // Request greeting messages for the names.
+    messages, err := greetings.Hellos(names)
     if err != nil {
         log.Fatal(err)
     }
-
-    // If no error was returned, print the returned message
-    // to the console.
-    fmt.Println(message)
+    // If no error was returned, print the returned map of
+    // messages to the console.
+    fmt.Println(messages)
 }
